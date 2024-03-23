c57dbaff19697e45ba1f0cdd6cf7cce36e61541d
Yo <franjomor@gmail.com>
2024-03-23 16:58:09 +0100

    feat: exercise 5 done. Using object to store the application information

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,27 +3,29 @@ import Content from './Content.jsx'
 import Total from './Total.jsx'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course}/>
-      <Content content={parts}/>
-      <Total sum={parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
+      <Header course={course.name}/>
+      <Content content={course.parts}/>
+      <Total sum={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}/>
     </div>
   )
 }
