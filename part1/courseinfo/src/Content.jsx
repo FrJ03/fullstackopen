94b088c1cfc6636b0f3efb4118d7c682914260a0
Yo <franjomor@gmail.com>
2024-03-23 16:53:48 +0100

    feat: exercise 3 done. Using objects to pass the application pats

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -3,9 +3,9 @@ import Part from './Part.jsx'
 const Content = (props) =>{
     return (
         <div>
-            <Part part={props.content[0].part} exercises={props.content[0].exercises}/>
-            <Part part={props.content[1].part} exercises={props.content[1].exercises}/>
-            <Part part={props.content[2].part} exercises={props.content[2].exercises}/>
+            <Part name={props.content[0].name} exercises={props.content[0].exercises}/>
+            <Part name={props.content[1].name} exercises={props.content[1].exercises}/>
+            <Part name={props.content[2].name} exercises={props.content[2].exercises}/>
         </div>
     )
 }
