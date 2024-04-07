f4eddbb22af1ee0ccfde4481a19741c2f4197a91
Yo <franjomor@gmail.com>
2024-04-07 11:51:44 +0200

    feat: exercise 2.6 done. Adding a list of names

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handlerNameChange = (event) => 
+    setNewName(event.target.value)
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      name: newName
+    }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handlerNameChange}/>
+        </div>
+        <div>
+          <button type="submit" onClick={addPerson}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person, i) =>
+        <p key={i}>{person.name}</p>
+      )}
+    </div>
+  )
+}
+
+export default App
