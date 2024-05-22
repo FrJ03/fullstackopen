0a9b4c1a26820f71a7e59816241ba172a274f89d
Yo <franjomor@gmail.com>
2024-05-22 17:27:02 +0200

    feat: exercise 4.20 done: token extractor middleware

--- part4/app.js
+++ part4/app.js
@@ -6,12 +6,14 @@ const mongoose = require('mongoose')
 const blogsRouter = require('./controller/blogs')
 const usersRouter = require('./controller/users')
 const loginRouter = require('./controller/login')
+const middleware = require('./utils/middleware')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
