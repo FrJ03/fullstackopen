c8abcff9839b2b74a053b82210a15dbde021536c
Yo <franjomor@gmail.com>
2024-05-23 20:02:43 +0200

    feat: exercise 3.14 done: url and likes test

--- part5/app/tests/Blog.test.jsx
+++ part5/app/tests/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from '../src/components/Blog'
 
 test('Renders blog title and blog author', () => {
@@ -7,6 +8,7 @@ test('Renders blog title and blog author', () => {
     author: 'author',
     id: 'id',
     likes: 1,
+    url: 'url',
     user: {
       id: 'id',
       username: 'username',
@@ -20,4 +22,30 @@ test('Renders blog title and blog author', () => {
 
   expect(container).toHaveTextContent('title')
   expect(container).toHaveTextContent('author')
+})
+
+test('Renders blog url and likes author', async () => {
+  const blog = {
+    title: 'title',
+    author: 'author',
+    id: 'id',
+    likes: 1,
+    url: 'url',
+    user: {
+      id: 'id',
+      username: 'username',
+      name: 'name'
+    }
+  }
+  const deleteBlog = () => 1
+  const sortBlogs = () => 1
+
+  const { container } = render(<Blog blog={blog} deleteBlog={deleteBlog} sortBlogs={sortBlogs}/>)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('show')
+  await user.click(button)
+
+  expect(container).toHaveTextContent('url')
+  expect(container).toHaveTextContent('1')
 })
